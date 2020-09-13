04a11fde18544d226b29063bf8603a1126e8b188
Vinh Trung Thai <Trung_thai@live.dk>
2020-09-13 22:34:15 +0200

    Added a bit to consumer and producer

--- Assignment1/Exercise3_3/Consumer.h
+++ Assignment1/Exercise3_3/Consumer.h
@@ -3,19 +3,26 @@ using namespace std;
 #include <systemc.h>
 #include "TCPHeader.h"
 
+template <class T>
 SC_MODULE(Consumer) {
 
-    sc_fifo<double> tcp_in;
+    sc_fifo_in_if<TCPHeader> tcp_in;
 
     SC_CTOR(Consumer) {
         SC_THREAD(consumer_thread);
     }
     void consumer_thread(void)
     {
+        T sample;
+
         while(1)
         {
-        	
+
         }
+    }
+    void print_consumer(void)
+    {
+        cout << tcp_in << endl;
     }
 };
 
--- Assignment1/Exercise3_3/Producer.h
+++ Assignment1/Exercise3_3/Producer.h
@@ -5,15 +5,23 @@ using namespace std;
 
 SC_MODULE(Producer) {
 
-    sc_fifo<double> tcp_out;
+    sc_port<sc_fifo_out_if<TCPHeader*>, 0> out;
 
     SC_CTOR(Producer) {
         SC_THREAD(producer_thread);
     }
     void producer_thread(void)
     {
+        TCPHeader package;
+
     	while(1)
     	{
+            int random = rand() % (10 - 2 + 1) + 2;
+            wait(random, SC_MS);
+            for (int i = 0; i < out.size(); i++)
+            {
+                out[i]->write(&package);
+            }
 
     	}
     }
