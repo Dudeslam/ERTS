9009b9d34c71ae73ca319e2b83219449940e06c1
Vinh Trung Thai <Trung_thai@live.dk>
2020-09-27 15:21:05 +0200

    Exercise 4 project

---
+++ Assignment2/lab4/Exercise_4/Exercise_4.sdk/Lab44/src/Main.c
@@ -0,0 +1,18 @@
+#include "xparameters.h"
+#include "xgpio.h"
+
+#include "led_ip.h"
+#include "xscutimer.h"
+#include "xscugic.h"
+#include "Matrixmul.h"
+#include "matrix_ip.h"
+#include "malloc.h"
+
+int main(void)
+{
+
+	while(1)
+	{
+
+	}
+}
